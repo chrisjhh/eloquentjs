102f39bdb3ec846c85d3c373c24afcac8dc68a21
Chris Hamer-Hodges <chris.hhodges@gmail.com>
2018-07-31 21:05:40 +0100

    Vector class

---
+++ test/objects.js
@@ -0,0 +1,38 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+class Vec {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(vector) {
+    return new Vec(this.x + vector.x, this.y + vector.y);
+  }
+
+  minus(vector) {
+    return new Vec(this.x - vector.x, this.y - vector.y);
+  }
+
+  get length() {
+    return Math.sqrt(this.x*this.x + this.y*this.y);
+  }
+   
+}
+
+describe.only('Objects', function() {
+  describe('vector', function() {
+    it('plus', function() {
+      expect(new Vec(1,2).plus(new Vec(2,3)))
+        .to.deep.equal({x: 3, y:5});
+    });
+    it('minus', function() {
+      expect(new Vec(1,2).minus(new Vec(2,3)))
+        .to.deep.equal({x: -1, y: -1});
+    });
+    it('length', function() {
+      expect(new Vec(3,4).length).to.equal(5);
+    });
+  });
+});
